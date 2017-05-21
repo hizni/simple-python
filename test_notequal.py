fd0f3244904d06aeead842f26d8521813d33c150
hizni.salih <hizni.salih@gmail.com>
2017-05-22 00:02:48 +0100

    added second test

---
+++ test_notequal.py
@@ -0,0 +1,14 @@
+
+import unittest
+
+class InequalityTest(unittest.TestCase):
+
+    def test_equal(self):
+        self.assertNotEqual(1, 2)
+
+    def test_not_equal(self):
+        self.assertEqual(2,2)
+
+
+if __name__ == '__main__':
+    unittest.main()
